1e1c6e720228bfdef5e75759e528f50ef11997d8
amitdeepvij <63993250+amitdeepvij@users.noreply.github.com>
2025-07-03 15:27:49 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,7 @@
 #output "instance_ami" {
-#  value = aws_instance.web.ami
+#  value = gcp_instance.web.ami
 #}
 
 #output "instance_arn" {
-#  value = aws_instance.web.arn
+#  value = gcp_instance.web.arn
 #}
